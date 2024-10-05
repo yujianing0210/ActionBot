2dfd688ff4283b9890619e5bf4b51bff577ee3e4
Jianing <100139737+yujianing0210@users.noreply.github.com>
2024-10-05 12:28:01 -0400

    Update client.js
    glitch

--- tutorial-echo/client-simple/client.js
+++ tutorial-echo/client-simple/client.js
@@ -1,8 +1,13 @@
 const WebSocket = require('ws');  // Import the WebSocket library
 
-const serverAddress = "ws://127.0.0.1:5002";
+// const serverAddress = "ws://127.0.0.1:5002";
+const serverAddress = "wss://grey-achieved-cyclamen.glitch.me/";
 
-const ws = new WebSocket(serverAddress);
+const ws = new WebSocket(serverAddress, {
+    headers: {
+        "User-Agent": "Google Chrome"
+    }
+});
 
 ws.on('open', function (){
     ws.send("hello, server");
